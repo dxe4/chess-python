4f0990d346638c3cb27969cce25fd51b9c8a0aa1
charalambosp <papaloizouc@gmail.com>
2013-12-10 20:58:56 +0000

    added test file

--- moves.py
+++ moves.py
@@ -33,11 +33,3 @@ def rook(x, y):
 def bishop(x, y):
     possible = lambda k: [(x + k, y + k), (x + k, y - k), (x - k, y + k), (x - k, y - k)]
     return {j for i in range(1, 8) for j in possible(i) if check_range(j)}
-
-
-for k in range(0, 8):
-    print(k, [i for i in range(0, 8)])
-
-assert knight(5, 4) == {(7, 3), (7, 5), (3, 3), (6, 6), (6, 2), (4, 2), (3, 5), (4, 6)}
-assert rook(5, 5) == {(5, 4), (1, 5), (5, 6), (5, 7), (4, 5), (7, 5), (0, 5), (5, 0), (5, 1), (2, 5), (3, 5), (5, 2), (6, 5), (5, 3)}
-assert bishop(2, 2) == {(1, 3), (3, 3), (6, 6), (5, 5), (3, 1), (4, 4), (7, 7), (0, 0), (0, 4), (1, 1), (4, 0)}
