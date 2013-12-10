95ca099e3b5650dd303d292f56e2d05ac7caa012
charalambosp <papaloizouc@gmail.com>
2013-12-10 20:57:43 +0000

    rename names for memory efficiency

--- chess.py
+++ chess.py
@@ -1,6 +1,7 @@
 from itertools import product
 from collections import OrderedDict
 
+#r rook, k knight, b bishop, q queen, k king
 
 def create_board(player_down="w"):
     board = {i: None for i in product(range(0, 8), range(0, 8))}
@@ -15,11 +16,11 @@ def create_board(player_down="w"):
         return filter(lambda x: x[1] == row, board.keys()),color_picker(row)
 
     def add_pawns(keys, color:str):
-        for i in keys: board[i] = (color, "pawn")
+        for i in keys: board[i] = (color, "p")
 
     def add_other(keys, color:str):
         keys = sorted(keys, key=lambda x: x[0])
-        pieces = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"]
+        pieces = ["r", "k", "b", "q", "k", "b", "k", "r"]
         for count, i in enumerate(keys): board[i] = (color,pieces[count])
 
     add_pawns(*get_row(1))
