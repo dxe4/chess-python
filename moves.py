157431aef2e2b2037f4fc723b2128b4443991d39
charalambosp <papaloizouc@gmail.com>
2013-12-10 21:29:34 +0000

    added moves.direction to detect where we are moving and tests for it

--- moves.py
+++ moves.py
@@ -1,7 +1,9 @@
 from itertools import product
 from itertools import chain
 from functools import wraps
+from math import atan2
 
+_angle_to_direction = {90:"up",-90:"down",180:"right",0:"left",-135:"right_down",135:"right_up",45:"left_up",-45:"left_down"}
 
 def move(f):
     @wraps(f)
@@ -18,18 +20,55 @@ def check_range(x):
 
 
 @move
-def knight(x, y):
+def _knight(x, y):
     moves = chain(product([x - 1, x + 1], [y - 2, y + 2]), product([x - 2, x + 2], [y - 1, y + 1]))
     moves = {(x, y) for x, y in moves if check_range((x, y))}
     return moves
 
 
 @move
-def rook(x, y):
+def _rook(x, y):
     return {(x, i) for i in range(0, 8)}.union({(i, y) for i in range(0, 8)})
 
 
 @move
-def bishop(x, y):
+def _bishop(x, y):
     possible = lambda k: [(x + k, y + k), (x + k, y - k), (x - k, y + k), (x - k, y - k)]
     return {j for i in range(1, 8) for j in possible(i) if check_range(j)}
+
+
+def direction(start,end):
+    delta_a = start[1] - end[1]
+    delta_b = start[0] - end[0]
+    return _angle_to_direction[int(atan2(delta_a, delta_b) * 180 / 3.14)]
+
+
+def _blocks(start,end,pattern):
+    pass
+
+def knight(start,end,board):
+    possible = _knight(*start)
+
+def rook(start,end,board):
+    possible = _rook(*start)
+    print(possible)
+
+def bishop(start,end,board):
+    possible = _bishop(*start)
+
+
+rook((3,3),(3,7),None)
+
+
+
+
+#0y [0, 1, 2, 3, 4, 5, 6, 7]x
+#1y [0, 1, 2, 3, 4, 5, 6, 7]x
+#2y [0, 1, 2, 3, 4, 5, 6, 7]x
+#3y [0, 1, 2, 3, 4, 5, 6, 7]x
+#4y [0, 1, 2, 3, 4, 5, 6, 7]x
+#5y [0, 1, 2, 3, 4, 5, 6, 7]x
+#6y [0, 1, 2, 3, 4, 5, 6, 7]x
+#7y [0, 1, 2, 3, 4, 5, 6, 7]x
+
+
--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 import moves
 
-for k in range(0, 8):
-    print(k, [i for i in range(0, 8)])
+assert moves._knight(5, 4) == {(7, 3), (7, 5), (3, 3), (6, 6), (6, 2), (4, 2), (3, 5), (4, 6)}
+assert moves._rook(5, 5) == {(5, 4), (1, 5), (5, 6), (5, 7), (4, 5), (7, 5), (0, 5), (5, 0), (5, 1), (2, 5), (3, 5), (5, 2), (6, 5), (5, 3)}
+assert moves._bishop(2, 2) == {(1, 3), (3, 3), (6, 6), (5, 5), (3, 1), (4, 4), (7, 7), (0, 0), (0, 4), (1, 1), (4, 0)}
 
-assert moves.knight(5, 4) == {(7, 3), (7, 5), (3, 3), (6, 6), (6, 2), (4, 2), (3, 5), (4, 6)}
-assert moves.rook(5, 5) == {(5, 4), (1, 5), (5, 6), (5, 7), (4, 5), (7, 5), (0, 5), (5, 0), (5, 1), (2, 5), (3, 5), (5, 2), (6, 5), (5, 3)}
-assert moves.bishop(2, 2) == {(1, 3), (3, 3), (6, 6), (5, 5), (3, 1), (4, 4), (7, 7), (0, 0), (0, 4), (1, 1), (4, 0)}
+assert moves.direction((3,3),(3,0)) == "up" #90
+assert moves.direction((3,3),(3,7)) == "down" #-90
+assert moves.direction((0,0),(7,0)) == "right" #180
+assert moves.direction((7,0),(0,0)) == "left" #0
+assert moves.direction((0,0),(3,3)) == "right_down" #-135
+assert moves.direction((3,3),(6,0)) == "right_up" #135
+assert moves.direction((3,3),(0,0)) == "left_up" #45
+assert moves.direction((3,3),(0,6)) == "left_down" #-45
