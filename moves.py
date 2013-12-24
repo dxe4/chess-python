f6b1d85cc246217253beb3a94a83d0e07812b561
charalambosp <papaloizouc@gmail.com>
2013-12-24 00:02:56 +0000

    change a bit the logic after a code review in stack exchange

--- moves.py
+++ moves.py
@@ -1,41 +1,25 @@
 from itertools import product
 from itertools import chain
 from functools import wraps
-from math import atan2
 
-#_angle_to_direction = {90: "up", -90: "down", 180: "right", 0: "left", -135: "right_down", 135: "right_up",45: "left_up", -45: "left_down"}
 
-_angle_to_direction = {
-    90: lambda x: (x[0], x[1] - 1),
-    -90: lambda x: (x[0], x[1] + 1),
-    180: lambda x: (x[0] + 1, x[1]),
-    0: lambda x: (x[0] - 1, x[1]),
-    -135: lambda x: (x[0] + 1, x[1] + 1),
-    135: lambda x: (x[0] + 1, x[1] - 1),
-    45: lambda x: (x[0] - 1, x[1] - 1),
-    -45: lambda x: (x[0] - 1, x[1] + 1)
-}
+def check_range(x:tuple, min=0, max=8) -> bool:
+    return min <= x[0] < max and min <= x[1] < max
+
 
 def move(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        x, y = args
-        moves = f(x, y)
-        if (x, y) in moves: moves.remove((x, y))
-        return moves
+        moves = f(*args, **kwargs)
+        return {i for i in moves if check_range(i)} - {args}
 
     return wrapper
-
-
-def check_range(x:tuple) -> bool:
-    return x[0] >= 0 and x[1] >= 0 and x[0] < 8 and x[1] < 8
 
 
 @move
 def _knight(x:int, y:int) -> set:
     moves = chain(product([x - 1, x + 1], [y - 2, y + 2]), product([x - 2, x + 2], [y - 1, y + 1]))
-    moves = {(x, y) for x, y in moves if check_range((x, y))}
-    return moves
+    return {(x, y) for x, y in moves}
 
 
 @move
@@ -46,35 +30,33 @@ def _rook(x:int, y:int) -> set:
 @move
 def _bishop(x:int, y:int) -> set:
     possible = lambda k: [(x + k, y + k), (x + k, y - k), (x - k, y + k), (x - k, y - k)]
-    return {j for i in range(1, 8) for j in possible(i) if check_range(j)}
-
-
-def _direction(start:tuple, end:tuple):
-    delta_a = start[1] - end[1]
-    delta_b = start[0] - end[0]
-    return _angle_to_direction[int(atan2(delta_a, delta_b) * 180 / 3.14)]
-
-
-def _blocks(start:tuple, end:tuple, pattern) -> bool:
-    pass
+    return {j for i in range(1, 8) for j in possible(i)}
 
 
 def knight(start:tuple, end:tuple, board:dict) -> tuple:
-    possible = _knight(*start)
+    return _knight(*start)
 
 
 def rook(start:tuple, end:tuple, board:dict) -> tuple:
-    possible = _rook(*start)
-    direction = _direction(start, end)
-    print(direction)
-    print(possible)
+    return _rook(*start)
 
 
-def bishop(start, end, board):
-    possible = _bishop(*start)
+def bishop(start:tuple, end:tuple, board:dict):
+    return _bishop(*start)
 
 
-rook((3, 3), (3, 7), None)
+def pawn(start:tuple, end:tuple, board:dict, player_pos:str) -> set:
+    result = set()
+    return {()}
+
+
+def _king(x:int, y:int) -> set:
+    pass
+
+
+print(bishop((3, 3), (5, 5), {}))
+print(rook((0, 0), (7, 0), {}))
+
 
 
 #0y [0, 1, 2, 3, 4, 5, 6, 7]x
