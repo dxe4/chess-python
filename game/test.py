01cf2b8fe54292cfa290e5200c9a77429dd3be9f
charalambosp <papaloizouc@gmail.com>
2013-12-29 02:38:14 +0000

    changed color to capital letter

--- game/test.py
+++ game/test.py
@@ -10,25 +10,25 @@ def test():
     unittest.main()
 
 
-bishop = Bishop("w", (3, 3))
+bishop = Bishop("W", (3, 3))
 _ = bishop.move((5, 5), {})
 print(_)
 
-rook = Rook("w", (0, 0))
+rook = Rook("W", (0, 0))
 _ = rook.move((7, 0), {})
 print(_)
 
-rook = Rook("w", (7, 0))
+rook = Rook("W", (7, 0))
 _ = rook.move((0, 0), {})
 print(_)
 
-queen = Queen("w", (0, 0))
+queen = Queen("W", (0, 0))
 _ = queen.move((7, 0), {})
 print(_)
 
 _ = queen.move((5, 5), {})
 print(_)
 
-knight = Knight("w", (0, 0))
+knight = Knight("W", (0, 0))
 _ = knight.move((2, 1), {})
 print(_)
