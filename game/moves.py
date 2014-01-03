2f150c554fbe8e49e9d7e978a34e39ef9c6b2aa1
charalambosp <papaloizouc@gmail.com>
2014-01-03 00:32:43 +0000

    fixed an issue with pawn

--- game/moves.py
+++ game/moves.py
@@ -243,8 +243,9 @@ class Pawn(Piece):
         return filtered_forward_moves
 
     def check_move(self, end: tuple, board: OrderedDict):
-        return self.find(*self.position, board=board)
-
+        if end in self.find(*self.position, board=board):
+            return self.find(*self.position, board=board)
+        return False
 
 class King(Piece):
 
