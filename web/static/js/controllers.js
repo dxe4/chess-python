2956a7160f893223a9198d86f9033e1735fbaba2
charalambosp <papaloizouc@gmail.com>
2014-01-26 22:25:33 +0000

    draw board

--- web/static/js/controllers.js
+++ web/static/js/controllers.js
@@ -1,11 +1,16 @@
 var myApp = angular.module('chess', []);
 var image_type = ".png";
 
+
+
 myApp.controller('CanvasCtrl', ['$scope', '$log', '$http',
     function ($scope, $log, $http) {
 
         var canvas = document.getElementById('canvas');
         var context = canvas.getContext('2d');
+
+        var piece_size = 80;
+        var board_size = piece_size * 8;
 
         var white_pieces = ['wK', 'wQ', 'wR', 'wB', 'wN', 'wP'];
         var black_pieces = ['bK', 'bQ', 'bR', 'bB', 'bN', 'bP'];
@@ -30,25 +35,46 @@ myApp.controller('CanvasCtrl', ['$scope', '$log', '$http',
             );
         };
 
+        $scope.draw_board = function () {
+            for (var i = 0; i < 8; i++) {
+                for (var j = 0; j < 8; j++) {
+                    context.moveTo(0, piece_size * j);
+                    context.lineTo(board_size, piece_size * j);
+                    context.stroke();
+                    context.moveTo(piece_size * i, 0);
+                    context.lineTo(piece_size * i, board_size);
+                    context.stroke();
+                    var left = 0;
+                    for (var a = 0; a < 8; a++) {
+                        for (var b = 0; b < 8; b += 2) {
+                            var startX = b * piece_size;
+                            if (a % 2 == 0) startX = (b + 1) * piece_size;
+                            context.fillRect(startX + left, (a * piece_size), piece_size, piece_size);
+                        }
+                    }
+                }
+            }
+        };
+
         $scope.get_images = function () {
             return document.getElementById("piece_images").children;
         };
 
         $scope.drawImage = function (image) {
             $log.info(image);
-            image.onload = function() {
+            image.onload = function () {
                 context.drawImage(image, 0, 0, image.width, image.height);
             };
 
         };
 
-        $scope.resize_canvas = function(x, y) {
-            canvas.width = x;
-            canvas.height = y;
+        $scope.resize_canvas = function () {
+            canvas.width = board_size;
+            canvas.height = board_size;
         };
 
         $scope._init = function () {
-            $scope.resize_canvas(600, 600);
+            $scope.resize_canvas();
             $http({method: 'GET', url: '/api/initial_board'}).
                 success(function (data, status, headers, config) {
                     $scope.data = data["values"];
@@ -60,6 +86,7 @@ myApp.controller('CanvasCtrl', ['$scope', '$log', '$http',
             context.globalAlpha = 1.0;
             context.beginPath();
             $scope.draw($scope.data);
+            $scope.draw_board();
         };
     }]);
 
