01cc948bde7f1885efceb932535ad971980b2588
charalambosp <papaloizouc@gmail.com>
2014-03-02 03:54:40 +0000

    added redis

--- api/__init__.py
+++ api/__init__.py
@@ -6,4 +6,7 @@ api_app = Flask(__name__)
 api_app.config.from_object(config)
 api_db = SQLAlchemy(api_app)
 
+from redis import Redis
+redis = Redis(host='localhost', port=6379, db=0)
+
 from api import application
