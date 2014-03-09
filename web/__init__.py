f6ed2e8f9980004011df3a0fd7b4ebf88aa796f2
charalambosp <papaloizouc@gmail.com>
2014-03-09 21:15:01 +0000

    signals

--- web/__init__.py
+++ web/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask, session
 from web import config
 from flask_login import LoginManager
 from web.models import User
-from flask_login import user_logged_in, current_user
+from flask_login import user_logged_in, user_logged_out, current_user, session_protected
 
 web_app = Flask(__name__)
 web_app.config.from_object(config)
@@ -17,15 +17,28 @@ login_manager.init_app(web_app)
 
 
 def new_login(sender, **extra):
-    print(sender)
-    print(extra)
+    print("login", sender, str(extra))
     print(current_user)
     print(session)
     session.permanent = True
     web_app.permanent_session_lifetime = timedelta(minutes=1)
 
 
+def new_logout(sender, **extra):
+    print("logout", str(sender), str(extra))
+
+
+def new_logout(sender, **extra):
+    print("logout", str(sender), str(extra))
+
+
+def new_expired_session(sender, **extra):
+    print("session", str(sender), str(extra))
+
+
+session_protected.connect(new_expired_session, web_app)
 user_logged_in.connect(new_login, web_app)
+user_logged_out.connect(new_logout, web_app)
 
 
 @login_manager.user_loader
