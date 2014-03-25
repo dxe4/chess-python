bb32fde3627a82a0c2569fd6dbd0bce71988cf45
charalambosp <papaloizouc@gmail.com>
2014-03-25 06:36:31 +0000

    changed login to modal

--- web/static/js/controllers/navbar_ctrl.js
+++ web/static/js/controllers/navbar_ctrl.js
@@ -1,25 +1,37 @@
 var chess = angular.module('chess');
 chess.controller('NavBarCtrl',
-    function ($scope, $cookies, $rootScope, LoginService) {
+    function ($scope, $cookies, $rootScope, $modal, LoginService) {
 
 
         $scope.login = function (_username) {
             LoginService.login(_username);
         };
 
-        $scope.logout = function(){
+        $scope.logout = function () {
             LoginService.logout();
         };
 
 
         $scope.init = function () {
-            if ($cookies.username){
+            if ($cookies.username) {
                 $rootScope.logged_in = $cookies.username;
             }
         };
         $scope.dropdown_clicked = function (e) {
             e.preventDefault();
             e.stopPropagation();
+        };
+
+        $scope.sign_in_clicked = function () {
+            var modalInstance = $modal.open({
+                templateUrl: 'templates/modal.html',
+                //controller: ModalInstanceCtrl,
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
         };
 
     });
