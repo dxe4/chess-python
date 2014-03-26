4874aed5e095a94baa2ebf18ab378dd3dc560ab6
charalambosp <papaloizouc@gmail.com>
2014-03-26 23:12:05 +0000

    re-invent the login

--- web/static/js/controllers/navbarCtrl.js
+++ web/static/js/controllers/navbarCtrl.js
@@ -1,10 +1,49 @@
 var chess = angular.module('chess');
 chess.controller('NavBarCtrl',
-    function ($scope, $cookies, $rootScope, $modal, UserService) {
+    function ($scope, $cookies, $rootScope, $log, $modal, UserService) {
 
+        var ModalInstanceCtrl = function ($scope, $modalInstance, sign_up) {
 
-        $scope.login = function (_username) {
-            UserService.login(_username);
+            var constructor = function () {
+
+                if (sign_up) {
+                    $scope.modal_title = "Sign Up!";
+                } else {
+                    $scope.modal_title = "Sign In!";
+                }
+
+            };
+            constructor();
+
+            $modalInstance.result.then(function () {
+            }, function () {
+            });
+
+            $scope._close = function () {
+                $modalInstance.close();
+            };
+
+            $scope.login = function (username) {
+                UserService.login(username);
+                $modalInstance.close();
+            };
+
+            $scope.register = function (data) {
+
+            };
+
+        };
+
+        var popUp = function (_sign_up) {
+            $modal.open({
+                templateUrl: 'templates/modal.html',
+                controller: ModalInstanceCtrl,
+                resolve: {
+                    sign_up: function () {
+                        return _sign_up;
+                    }
+                }
+            });
         };
 
         $scope.logout = function () {
@@ -17,44 +56,13 @@ chess.controller('NavBarCtrl',
                 $rootScope.logged_in = $cookies.username;
             }
         };
-        $scope.dropdown_clicked = function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-        };
 
         $scope.sign_in_clicked = function () {
-            $scope.register = false;
-            $scope.sign_in = true;
-            var modalInstance = $modal.open({
-                templateUrl: 'templates/modal.html',
-                controller: 'NavBarCtrl'
-//                resolve: { items: function () { return $scope.items; }}
-            });
+            popUp(false);
         };
 
-        $scope.register_clicked = function () {
-            $scope.sign_in = false;
-            $scope.register = true;
-            var modalInstance = $modal.open({
-                templateUrl: 'templates/modal.html',
-                controller: 'NavBarCtrl'
-//                resolve: { items: function () { return $scope.items; }}
-            });
-        };
-
-        $scope.login = function (username) {
-
-        };
-
-        $scope.register = function (data) {
-
+        $scope.sign_up_clicked = function () {
+            popUp(true);
         };
 
     });
-
-
-//myApp.controller('TestCtrl', ['$scope', '$log', '$http',
-//    function ($scope, $log, $http) {
-//        //use only for testing
-//    }]);
-
