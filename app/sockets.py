6ff3bc9b48c04d8858cfb5b27fd8f10a8d4fcb13
charalambosp <papaloizouc@gmail.com>
2014-04-07 23:44:50 +0100

    proper socket

--- app/sockets.py
+++ app/sockets.py
@@ -1,10 +1,24 @@
 import cherrypy
 from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
-from ws4py.websocket import EchoWebSocket
+from ws4py.websocket import WebSocket
+
+
+class CoolSocket(WebSocket):
+    def opened(self):
+        print("socket opened", self)
+
+    def closed(self, code, reason=None):
+        print("socket closed", self)
+
+    def received_message(self, message):
+        print("received", message)
+        self.send(message.data, message.is_binary)
+
 
 cherrypy.config.update({'server.socket_port': 9000})
 WebSocketPlugin(cherrypy.engine).subscribe()
 cherrypy.tools.websocket = WebSocketTool()
+
 
 class Root(object):
     @cherrypy.expose
@@ -13,8 +27,8 @@ class Root(object):
 
     @cherrypy.expose
     def ws(self):
-        # you can access the class instance through
-        handler = cherrypy.request.ws_handler
+        cool_socket = cherrypy.request.ws_handler
+
 
 cherrypy.quickstart(Root(), '/', config={'/ws': {'tools.websocket.on': True,
-                                                 'tools.websocket.handler_cls': EchoWebSocket}})
+                                                 'tools.websocket.handler_cls': CoolSocket}})
