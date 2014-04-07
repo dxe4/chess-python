77b52f4c099891793e289d1e2d076e23661ba6c2
charalambosp <papaloizouc@gmail.com>
2014-04-07 23:24:29 +0100

    deleted flask :*(

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 source venv/bin/activate
 
-echo "staring websocket server"
-python sockets/server.py &
-
 echo "starting redis"
 redis-server
 
 echo "starting gunicorn"
-gunicorn -b 127.0.0.1:8080 -w 4 main:application &
+python app/application.py
--- web/config.py
+++
@@ -1,3 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:root@raspberrypi/test"
