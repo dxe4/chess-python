9b529c51b0393d1f5251cc8123103bed550a9e49
charalambosp <papaloizouc@gmail.com>
2014-04-12 03:02:45 +0100

    environ.get

--- app/sockets.py
+++ app/sockets.py
@@ -3,6 +3,7 @@ import json
 from app import settings
 from common.redis_queue import RedisQueue
 from ws4py.websocket import WebSocket
+from multiprocessing import Process
 
 
 r_queue = RedisQueue("all_players", settings.REDIS_QUEUE_KWARGS)
