9148cf4aede01f2bda663c07cb17842b37478386
charalambosp <papaloizouc@gmail.com>
2014-04-13 18:17:16 +0100

    start redis tests

---
+++ common/tests.py
@@ -0,0 +1,19 @@
+import unittest
+from multiprocessing.pool import Pool
+from multiprocessing import Process
+from redis import StrictRedis
+from common import PubSubPool, RedisQueue
+
+
+class TestRedis(unittest.TestCase):
+    def setUp(self):
+        self.pool = PubSubPool(size=2)
+        self.queue = RedisQueue("test_q")
+
+    def test_board_init(self):
+        channel, pubsub = self.pool.join()
+        assert "channel" in channel
+        # pubsub.listen()
+
+if __name__ == '__main__':
+    unittest.main()
